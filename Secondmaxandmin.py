57cd5ea5c2e0baeb2d2752f64ba5749b2f6c255c
sunil <saisunilregulagadda@gmail.com>
2026-06-13 07:39:33 +0530

    ,

--- Secondmaxandmin.py
+++ Secondmaxandmin.py
@@ -1,7 +1,7 @@
 l=list(map(int,input().split()))
 l.sort()
 max=l[-1]
-for i in range(len(l)-2-1,-1,-1):
+for i in range(len(l)-2,-1,-1):
     if l[i]!=max:
         print("Second max is ",l[i])
         break
